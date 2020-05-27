e558389ad88041fcfc67a1fa1d4a86b83b3b35af
Rodolfo <rodolfotiago@gmail.com>
2020-05-27 14:02:01 -0300

    primeiro commit

---
+++ cloud/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" { }
+variable "region" { }
+variable "key" { }
+variable "vpc_name" { }
+variable "tf_backend_bucket_name" { }
+variable "service_list" {
+  type  = list
+}
+variable "subnet_name" { }
+variable "subnet_cidr" { }
+variable "firewall_name" { }
