5c09da749f7f872365a17801350e73e97a7bb169
Rodolfo <rodolfotiago@gmail.com>
2020-05-27 14:02:01 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # projeto-aplicado
+Repositório para manter o código referente ao meu projeto aplicado
---
+++ cloud/terraform/gcp-vpc.tf
@@ -0,0 +1,31 @@
+//Criar Virtual Private Cloud
+resource "google_compute_network" "vpc" {
+    name                    = var.vpc_name
+    auto_create_subnetworks = false
+}
+
+//Criar subnets
+resource "google_compute_subnetwork" "subnet" {
+    name            = var.subnet_name
+    ip_cidr_range   = var.subnet_cidr
+    network         = var.vpc_name
+    depends_on      = google_compute_network.vpc
+    region          = var.region
+}
+
+//Criar regra de firewall
+resource "google_compute_firewall" "firewall" {
+    name        = var.firewall_name
+    network     = google_compute_network.vpc.name
+    depends_on  = google_compute_subnet.subnet
+    
+    allow {
+        protocol    = "icmp"
+    }
+    allow {
+        protocol    = "tcp"
+        ports       = ["22"]
+    }
+
+    source_ranges   = ["0.0.0.0/0"]
+}
