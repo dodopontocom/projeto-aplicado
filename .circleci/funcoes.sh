b01d9c9c0e6744398c7dc95e556db6756dbffacf
rodolfotiago <rodolfotiago@gmail.com>
2020-05-28 16:48:15 -0300

    debug

---
+++ .circleci/funcoes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+helper.ambiente_vars() {
+  local env=$1
+  
+  vars=($(printenv | egrep -o "${env}_CI_.*=" | awk -F= '{print $1}'))
+
+  for var in "${vars[@]}"; do  
+    var=$(echo "${var}" | awk -F '=' '{print $1}')
+    new_var=$(echo "${var}" | cut -d'_' -f3-)
+    export "{new_var}"="${!var}"            
+  done
+}
