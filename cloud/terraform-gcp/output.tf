201e096135f869070da00342c04be5a06e390107
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-30 16:49:44 +0000

    [gcp-apply] centos instances cool

--- cloud/terraform-gcp/output.tf
+++ cloud/terraform-gcp/output.tf
@@ -1,4 +1,6 @@
-
-output "ip" {
-  value = google_compute_instance.instance.network_interface.0.access_config.0.nat_ip
+output "ubuntu_ip" {
+  value = google_compute_instance.ubuntu_instance.network_interface.0.access_config.0.nat_ip
+}
+output "centos_ip" {
+  value = google_compute_instance.centos_instance.network_interface.0.access_config.0.nat_ip
 }
